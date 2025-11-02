9cd1e40056e5677e71b8bf685c7d00cf8f754794
kimhji <twin4185@naver.com>
2025-11-02 23:32:42 +0900

    feat: Lotto 랜덤 값 추출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,4 +4,15 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public void buyLotto(Lotto[] lottos, int money){
+        if(money%1000 != 0) throw new IllegalArgumentException("[ERROR] 로또의 가격은 1000원입니다. 잔돈은 제외하고 천 원 단위로 입력해주세요.");
+        int count = money/1000;
+        if(count == 0) throw new IllegalArgumentException("[ERROR] 로또의 가격은 1000원입니다. 돈이 부족합니다.");
+
+        lottos = new Lotto[count];
+        for(int i = 0;i<count;i++){
+            lottos[i] = new Lotto();
+        }
+    }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,9 +14,20 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Lotto() {
+        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        Set<Integer> numberNoDup = new HashSet<Integer>();
+        for(Integer one: numbers){
+            numberNoDup.add(one);
+        }
+        if(numberNoDup.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되어선 안됩니다.");
         }
     }
 
