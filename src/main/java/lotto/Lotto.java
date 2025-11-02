7bc5ad1c84deb81ddc3a62f00c91c42296f73be0
kimhji <twin4185@naver.com>
2025-11-03 00:06:45 +0900

    feat: 당첨 순위 판단

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -32,6 +32,10 @@ public class Lotto {
         }
     }
 
+    public List<Integer> getLottoNumbers(){
+        return this.numbers;
+    }
+
     public static void isValidLottoNumber(int number){
         if(number<1 || number>45){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
@@ -47,5 +51,34 @@ public class Lotto {
             }
         }
         System.out.println("]");
+    }
+
+    public int getLottoRank(Lotto rightLotto, int bonusNumber){
+        int includeNum = 0;
+        boolean isIncludeBonusNumber = false;
+
+        for(int one : rightLotto.getLottoNumbers()){
+            if(this.isIncludeNumber(one)){
+                includeNum++;
+            }
+        }
+        isIncludeBonusNumber = this.isIncludeNumber(bonusNumber);
+        return getRankFromScore(includeNum, isIncludeBonusNumber);
+    }
+
+    public boolean isIncludeNumber(int number){
+        for(Integer one: this.numbers){
+            if(number == one) return true;
+        }
+        return false;
+    }
+
+    private int getRankFromScore(int includeNum, boolean isIncludeBonusNumber){
+        if(includeNum == 6) return 1;
+        if(includeNum == 5 && isIncludeBonusNumber) return 2;
+        if(includeNum == 5) return 3;
+        if(includeNum == 4) return 4;
+        if(includeNum == 3) return 5;
+        return -1;
     }
 }
